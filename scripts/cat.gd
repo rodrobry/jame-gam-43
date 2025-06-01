bd1d9510b2f5bfb9062b4f4c326892522f1ca167
paulomag2106 <paulomag2106@gmail.com>
2025-06-01 11:31:38 -0300

    added capsule distance calc function to properly detect distance from cat to player

--- scripts/cat.gd
+++ scripts/cat.gd
@@ -3,7 +3,7 @@ class_name Cat
 extends CharacterBody2D
 
 @export var speed: float = 25.0
-@export var attack_range: float = 20.0
+@export var attack_range: float = 5.0
 @onready var attack_timer: Timer = $Timer
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -24,7 +24,7 @@ func _physics_process(_delta: float) -> void:
 		
 	# Check direction and distance towards player
 	var direction = (player.global_position - global_position).normalized()
-	var distance = global_position.distance_to(player.global_position)
+	var distance = distance_to_capsule(global_position - player.global_position, 16, 6)
 	
 	if attack_on_cooldown and distance <= attack_range * 2:
 		return
@@ -51,6 +51,11 @@ func attack():
 	animated_sprite_2d.play("attack")
 	attack_timer.start(1.2)
 	attack_on_cooldown = true
+	
+func distance_to_capsule(pos: Vector2, height: float, radius: float) -> float:
+	var d : Vector2 = abs(pos) - Vector2(0, height);
+	d = Vector2(max(d.x, 0.0), max(d.y, 0.0))
+	return d.length() + min(max(d.x,d.y),0.0) - radius;
 
 func die():
 	# Emit death signal once
@@ -74,7 +79,6 @@ func die():
 			t.tween_property(self, "modulate:a", 0.0, 2.0).set_trans(Tween.TRANS_SINE)
 			t.play()
 			t.tween_callback(Callable(self, "_on_fade_complete"))
-			
 
 		# Set exit speed, run animation with custom speed, and remove collision component
 		speed = 75.0
